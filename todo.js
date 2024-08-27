dafdd5ff6acfa164e6225ee4dedcf2eb88d9b0dd
Charlotte Akongo <charlotte.akongo@gmail.com>
2024-08-27 15:28:54 +0300

    created todolist app

---
+++ todo.js
@@ -0,0 +1,43 @@
+const inputBox = document.getElementById("input-box");
+const addButton = document.getElementById("add-button");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+  inputBox.value === "";
+  saveData();
+
+  if (inputBox.value === "") {
+    alert("You must write soemthing");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+}
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
